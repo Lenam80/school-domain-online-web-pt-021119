e84f0988ed8e64814be1ac254e1d37996800c20f
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-03 02:15:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,3 +3,8 @@ def school.roster
 
 def school.add_student("Zach Morris", 9)
 def school.roster
+
+def  school.add_student("AC Slater", 9)
+def school.add_student("Kelly Kapowski", 10)
+def school.add_student("Screech", 11)
+def school.roster
